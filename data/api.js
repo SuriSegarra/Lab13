54dbef145793490a49013862d5896766319ac327
surelis segarra <sdarianne34@gmail.com>
2020-01-30 11:12:26 -0800

    added html and sj

---
+++ data/api.js
@@ -0,0 +1,13 @@
+//pop user in localStorage
+export function saveUser(user) {
+    //getting user as a string
+    const json = JSON.stringify(user);
+    localStorage.setItem('user', json);
+}
+
+export function getUser() {
+    const json = localStorage.getItem('user');
+    if (!json) return null;
+    const user = JSON.parse(json);
+    return user;
+}
